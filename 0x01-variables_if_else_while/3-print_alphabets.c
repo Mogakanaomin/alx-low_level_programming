be261310d9e2af0a477627952b5db8b20b85b7c5
mogakanomin <mogakanaomin@gmail.com>
2023-07-17 13:53:10 -0700

    Program that Prints the alphabet in lowercase then in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -  Program that Prints the alphabet.
+ *
+ * Return: 0 (Code Successful)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
